4562b33de03ec6e128368de2e23be98d272b6469
slowcloud <sju9417@gmail.com>
2023-09-28 18:45:52 +0900

    중복되는 코드 메소드로 리팩터링

--- src/main/java/kr/ac/pknu/capstone/ApiController.java
+++ src/main/java/kr/ac/pknu/capstone/ApiController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,14 +22,16 @@ public class ApiController {
     @Autowired
     ObjectMapper objectMapper;
 
-    @GetMapping("/")
-    public List<Data> root() throws Exception {
-        // 일단 데이터를 통짜로 넘기는 것부터
-
+    private List<Data> readJsonData() throws IOException {
         ClassPathResource resource = new ClassPathResource("data.json");
         File file = resource.getFile();
-        return objectMapper.readValue(file, new TypeReference<List<Data>>() {});
+        return objectMapper.readValue(file, new TypeReference<List<Data>>() { });
+    }
 
+    @GetMapping("/")
+    public List<Data> root() throws Exception {
+        // JPA로 옮기면 수정할 것
+        return readJsonData();
     }
 
     @GetMapping("query")
@@ -36,9 +39,7 @@ public class ApiController {
             @RequestParam(value = "vCPU", defaultValue = "0") Integer vcpu, @RequestParam(value = "Memory", defaultValue = "0") Integer memory
     ) throws Exception {
 
-        ClassPathResource resource = new ClassPathResource("data.json");
-        File file = resource.getFile();
-        List<Data> data = objectMapper.readValue(file, new TypeReference<List<Data>>() {});
+        List<Data> data = readJsonData();
 
         return data.stream().filter(d ->
                 (vcpu.equals(0) || d.getVCPU().equals(vcpu)) && (memory.equals(0) || d.getMemory().equals(memory))
