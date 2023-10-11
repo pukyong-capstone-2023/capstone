96c1d9d0f02052f734a4483d3790451e004c8965
slowcloud <sju9417@gmail.com>
2023-10-11 15:01:06 +0900

    test: 테스트 코드 이동
    서비스 테스트에 있던, 리포지토리에 대한 테스트코드를 옮겼고 단위 테스트에 더 알맞게 변경

---
+++ src/test/java/kr/ac/pknu/capstone/domain/DataRepositoryTest.java
@@ -0,0 +1,46 @@
+package kr.ac.pknu.capstone.domain;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.ac.pknu.capstone.domain.Data.Data;
+import kr.ac.pknu.capstone.domain.Data.DataRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DataRepositoryTest {
+
+    @Autowired
+    DataRepository dataRepository;
+
+    @DisplayName("json을 정상적으로 읽는지, 객체 매핑이 되는지 확인")
+    @Test
+    public void json_읽기() throws Exception {
+
+        List<Data> data = dataRepository.findAll();
+
+        Assertions.assertThat(data)
+                .isNotEmpty();
+
+    }
+
+    @DisplayName("data에서 필터링이 작동하는지 확인")
+    @Test
+    public void filterTest() throws Exception {
+
+        Integer vcpu = 2, memory = 4;
+        List<Data> res = dataRepository.search(vcpu, memory);
+
+        Assertions.assertThat(res)
+                .isNotEmpty()
+                .allMatch(d -> d.getVcpu().equals(vcpu) && d.getMemory().equals(memory));
+
+    }
+
+}
--- src/test/java/kr/ac/pknu/capstone/service/DataServiceTest.java
+++ src/test/java/kr/ac/pknu/capstone/service/DataServiceTest.java
@@ -3,6 +3,7 @@ package kr.ac.pknu.capstone.service;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.ac.pknu.capstone.domain.Data.Data;
+import kr.ac.pknu.capstone.domain.Data.DataRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,39 +18,5 @@ import java.util.stream.Collectors;
 @SpringBootTest
 public class DataServiceTest {
 
-    @Autowired
-    ObjectMapper objectMapper;
-
-    @DisplayName("json을 정상적으로 읽는지, 객체 매핑이 되는지 확인")
-    @Test
-    public void json_읽기() throws Exception {
-
-        ClassPathResource resource = new ClassPathResource("data.json");
-        File file = resource.getFile();
-        List<Data> data = objectMapper.readValue(file, new TypeReference<List<Data>>() {});
-
-        Assertions.assertThat(data)
-                .isNotEmpty();
-
-    }
-
-    @DisplayName("data에서 필터링이 작동하는지 확인")
-    @Test
-    public void filterTest() throws Exception {
-
-        ClassPathResource resource = new ClassPathResource("data.json");
-        File file = resource.getFile();
-        List<Data> data = objectMapper.readValue(file, new TypeReference<List<Data>>() {});
-
-        Integer vcpu = 2, memory = 4;
-        List<Data> res = data.stream().filter(d ->
-                (vcpu.equals(0) || d.getVcpu().equals(vcpu)) && (memory.equals(0) || d.getMemory().equals(memory))
-        ).collect(Collectors.toList());
-
-        Assertions.assertThat(res)
-                .isNotEmpty()
-                .allMatch(d -> d.getVcpu().equals(vcpu) && d.getMemory().equals(memory));
-
-    }
 
 }
