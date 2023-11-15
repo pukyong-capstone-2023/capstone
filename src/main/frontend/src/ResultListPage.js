3780c7155256501077bc405c4e831100e832827e
slowcloud <sju9417@gmail.com>
2023-11-15 20:57:16 +0900

    refactor: ResultListPage를 간결하게 수정
    - 내부 컴포넌트 리팩터링 작업 필요

--- src/main/frontend/src/ResultListPage.js
+++ src/main/frontend/src/ResultListPage.js
@@ -1,33 +1,18 @@
 import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
 import ContentList from "./component/ContentList";
 import Chart from './component/Chart';
 import AddContent from './component/AddContent';
 import Footer from './component/Footer';
-import titleImg from './img/pngegg.png';
 
 
 function ResultListPage() {
     return (
-        <div>
-            <div className="introduce_headerbar" style={{ textAlign: 'lte'}}>
-                <Navbar>
-                    <Container>
-                        <Navbar.Brand href="/" style={{ display:'flex' }}>
-                             <img src = {titleImg}/>
-                             <h1>Team 올린엄지</h1>
-                        </Navbar.Brand>
-                    </Container>
-                </Navbar>
-            </div>
-            <div className='result_contentbar'>
-                <ContentList />
-            </div>
+        <Container>
+            <ContentList />
             <Chart />
             <AddContent />
             <Footer />
-        </div>
-
+        </Container>
     );
 };
 
