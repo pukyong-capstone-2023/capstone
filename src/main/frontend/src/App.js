8935a3e179deebeddbfbce03cf242ca4eba10f8b
slowcloud <sju9417@gmail.com>
2023-11-15 20:53:59 +0900

    refactor: 부트스트랩이 제대로 적용되도록 변경

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {Route, Routes} from 'react-router-dom';
 import IntroducePage from './IntroducePage';
 import ResultListPage from './ResultListPage';
