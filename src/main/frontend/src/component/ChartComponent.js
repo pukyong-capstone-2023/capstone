f9b7fb4020e1c10595b1b6fcdb6ed7500ee33034
slowcloud <sju9417@gmail.com>
2023-11-18 16:04:13 +0900

    feat: 목업 데이터로 차트 구현

--- src/main/frontend/src/component/ChartComponent.js
+++ src/main/frontend/src/component/ChartComponent.js
@@ -1,39 +1,42 @@
-import data from '../data'
+import 'chart.js/auto'
+import { Line } from 'react-chartjs-2';
 
-function ChartComponent() {
+function ChartComponent({data}) {
 
-    // console.log(data)
+    console.log(data)
+
+    const mockup = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            type: 'line',
+            label: 'Dataset 1',
+            borderColor: 'rgb(54, 162, 235)',
+            borderWidth: 2,
+            data: [1, 2, 3, 4, 5],
+          },
+          {
+            type: 'bar',
+            label: 'Dataset 2',
+            backgroundColor: 'rgb(255, 99, 132)',
+            data: [1, 2, 3, 4, 5, 6],
+            borderColor: 'red',
+            borderWidth: 2,
+          },
+          {
+            type: 'bar',
+            label: 'Dataset 3',
+            backgroundColor: 'rgb(75, 192, 192)',
+            data: [1, 2, 3, 4, 5, 6],
+          },
+        ],
+      };
 
     return(
-        <>
-            <div className="content">
-                <div className="chart">
-                    <canvas id="chart" width="300" height="200"></canvas>
-                </div>
-            </div>
-        </>
+        <div className='col-12'>
+            <Line data={mockup} />
+        </div>
     );
 };
 
 export default ChartComponent;
-
-
-/*
-
-        axios.get("/api/")
-        .then(data => data.data)
-        .then(jsonData => {
-            jsonData.forEach((element) => {
-                if (element["Currency exchange rates"] === "USD") {
-                    element["Currency exchange rates"] = "WON";
-                    element["Cost per hour"] = Math.round(
-                    Number(element["Cost per hour"]) * 1290.45
-                    );
-                } else {
-                    element["Cost per hour"] = Number(element["Cost per hour"]);
-                }
-            });
-            return jsonData;
-        })
-//        .then(console.log)
-*/
