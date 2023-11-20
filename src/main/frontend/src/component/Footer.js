8012c2079555ed7f5c5a58babd37c76b923168eb
slowcloud <sju9417@gmail.com>
2023-11-20 20:44:23 +0900

    refactor: footer 수정

--- src/main/frontend/src/component/Footer.js
+++ src/main/frontend/src/component/Footer.js
@@ -1,26 +1,20 @@
-// import './Footer.css';
-// import '../IntroducePage.css';
-import { Navbar } from "react-bootstrap";
 
-
-function Footer() {
+export default function Footer() {
 
     return (
-            <Navbar className="text-bg-secondary p-3" style={{ display:'grid'}}>
-            <Navbar.Brand style={{marginLeft:"30px"}} >
+            <div className="container bg-dark bg-opacity-10 p-3">
+                <footer className="row">
+                    <div className="col">
                         <h5>Creaters</h5>
-                        <p style={{ paddingLeft: '20px', marginTop: '4px'}}>이건창, 서정운, 이나겸, 쑤와디</p>
-            </Navbar.Brand>
-            <Navbar.Brand style={{marginLeft:"30px"}} >
+                        <p>이건창, 서정운, 이나겸, 쑤와디</p>
+                    </div>
+                    <div className="col">
                         <h5>Github Link</h5>
-                        <p style={{ paddingLeft: '20px', marginTop: '4px' }}>http://...</p>
-            </Navbar.Brand>
-            <Navbar.Brand style={{marginLeft:"30px"}} >
-                        <h5>Develops</h5>
-                        <p style={{ paddingLeft: '20px', marginTop: '4px' }}>chart.js 사용..</p>
-            </Navbar.Brand>
-            </Navbar>
+                        <p>https://github.com/pukyong-capstone-2023/capstone</p>
+                    </div>
+                    <div className="col"/>
+                    <div className="col"/>
+                </footer>
+            </div>
     );
 };
-
-export default Footer;
